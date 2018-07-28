3f5fda8d6b43fe6fb8264d5fa9d8170f3c23b69d
Jionne Lizardo <kailizardo@gmail.com>
2018-07-28 19:19:05 +0800

    fix image case sensitive

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 
 $(".northbutton").on("mouseenter", function(){
-  $(".backgroundimage").attr("src", "img/5.JPG");
+  $(".backgroundimage").attr("src", "img/person5.jpg");
   console.log("im over the northbutton");
 });
 
 $(".northbutton").on("mouseleave", function(){
-  $(".backgroundimage").attr("src", "img/3.JPG");
+  $(".backgroundimage").attr("src", "img/person3.jpg");
 });
 
 $(".southbutton").on("mouseenter", function(){
-  $(".backgroundimage").attr("src", "img/lala.JPG");
+  $(".backgroundimage").attr("src", "img/lala.jpg");
 });
 
 $(".southbutton").on("mouseleave", function(){
-  $(".backgroundimage").attr("src", "img/3.JPG");
+  $(".backgroundimage").attr("src", "img/person3.jpg");
 });
 
 $(".rightbutton").on("mouseenter", function(){
-  $(".backgroundimage").attr("src", "img/4.JPG");
+  $(".backgroundimage").attr("src", "img/person4.jpg");
 });
 
 $(".rightbutton").on("mouseleave", function(){
-  $(".backgroundimage").attr("src", "img/3.JPG");
-});
+  $(".backgroundimage").attr("src", "img/person3.jpg");
+});jpg
 
 $(".leftbutton").on("mouseenter", function(){
-  $(".backgroundimage").attr("src", "img/1.JPG");
+  $(".backgroundimage").attr("src", "img/person1.jpg");
 });
 
 $(".leftbutton").on("mouseleave", function(){
-  $(".backgroundimage").attr("src", "img/3.JPG");
+  $(".backgroundimage").attr("src", "img/person3.jpg");
 });
